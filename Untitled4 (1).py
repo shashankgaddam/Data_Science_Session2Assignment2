65bdb12716c3f21a4c0c064bc550c4536ed59943
shashankgaddam <34601798+shashankgaddam@users.noreply.github.com>
2018-02-09 21:31:46 +0530

    Add files via upload

---
+++ Untitled4 (1).py
@@ -0,0 +1,15 @@
+
+# coding: utf-8
+
+# In[8]:
+
+
+for i in range(0,5):
+    for j in range(0,i):
+        print("*", end = " ")
+    print('')
+for i in range(5,0,-1):
+    for j in range(0,i):
+        print("*", end = " ")
+    print('')
+
